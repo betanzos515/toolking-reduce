893b96e64d43a37c560a13f86bf6bf9ab443e647
David Betanzos <betanzos515@gmail.com>
2023-01-01 14:14:01 -0600

    Subiendo cambios

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,17 @@
+
+const PokemonApp = () => {
+  return (
+    <>
+        <h1>PokemonApi</h1>
+        <hr />
+
+        <ul>
+            <li>Hola</li>
+            <li>Hola</li>
+            <li>Hola</li>
+        </ul>
+    </>
+  )
+}
+
+export default PokemonApp
